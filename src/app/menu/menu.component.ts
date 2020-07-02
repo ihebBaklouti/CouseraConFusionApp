24ecb827120a8cdbcd67025cb309cea2ff25db58
Iheb Baklouti <ihebbaklouti02@gmail.com>
2020-07-02 13:13:36 +0100

    Components Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Dish} from '../shared/dish';
 import { from } from 'rxjs';
-@Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
-})
-export class MenuComponent implements OnInit {
 
-dishes:Dish[]=[
+const DISHES: Dish[] = [
   {
     id: '0',
     name: 'Uthappizza',
@@ -50,7 +44,18 @@ dishes:Dish[]=[
     price: '2.99',
     description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
   }
-];
+  ];
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+dishes=DISHES;
+selectedDish = DISHES[0];
+
 
   constructor() { }
 
